9e9241f3e2afc7cdfe35773dce8a64db97728269
12nick12 <nicholasleffler@gmail.com>
2020-01-12 14:40:59 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -210,7 +210,7 @@ create_wp_db
 # Create site with wp-cli
 mkdir -p "${siteFP}" || exit
 cd "${siteFP}" || exit
-chown -R "${wwwUser}":"${wwwUser}" "$(dirname "${siteFP}")"
+chown -R "${wwwUser}":"${wwwUser}" "$(dirname ${siteFP})"
 sudo -u "${wwwUser}" /usr/local/bin/wp core download
 sudo -u "${wwwUser}" /usr/local/bin/wp config create --dbname="$(siteNoP}" --dbuser="$(siteNoP}" --dbpass="${wpasswd}" --path="${siteFP}"
 sudo -u "${wwwUser}" /usr/local/bin/wp core install --url="${siteURL}" --title="${siteTitle}" --admin_user="wpm258159" --admin_password="${wpapasswd}" --admin_email="${adminEmail}" --path="${siteFP}" --skip-email
